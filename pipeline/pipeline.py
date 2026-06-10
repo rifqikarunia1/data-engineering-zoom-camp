bad04f943676d9d8cc0dbe02fbedd19005253fa0
rifqikarunia1 <113981850+rifqikarunia1@users.noreply.github.com>
2026-06-10 12:16:08 +0000

    Add docker

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,5 +1,4 @@
 import sys
-
 import pandas as pd
 
 print("arguments", sys.argv)
@@ -8,12 +7,15 @@ day = int(sys.argv[1])
 month = int(sys.argv[2])
 
 head = pd.DataFrame({"day": [day], "month": [month]})
-# head.to_csv("output.csv", index=False)
 
-print(head.head())  
-
-df = pd.DataFrame({"day": [1,23,412], "month": [1,32,3]})
+df = pd.DataFrame({
+    "day": [1, 23, 412],
+    "month": [1, 32, 3]
+})
 
 print(df.head())
+
+# export parquet
+df.to_parquet("output.parquet", index=False)
 
 print(f"Running pipeline for day {day} and month {month}")
